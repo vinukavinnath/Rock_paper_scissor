0c31cb30de063b1e18671442eaa29d28ae8294b2
vinukavinnath <wlvvinnath@gmail.com>
2022-08-25 21:15:08 +0530

    2nd Commit_Dash complete

--- RockPaperScissor.cpp
+++ RockPaperScissor.cpp
@@ -4,14 +4,33 @@
 
 using namespace std;
 
-int main(){
-    return 0;
-}
-
 void dashboard(){
+    cout<<endl;
+    cout.width(50);
+    cout<<"-------------------------------------------"<<endl;
+    cout.width(50);
+    cout<<"    R O C K   p a p e r   S C I S S O R    "<<endl;
+    cout.width(50);
+    cout<<"-------------------------------------------"<<endl<<endl;
 
+    cout<<"Hi There! Let's play Rock Paper Scissor."<<endl<<"All you want to do is :-"<<endl;
+    cout<<"\t\t\t\"r\" for ROCK"<<endl;
+    cout<<"\t\t\t\"p\" for PAPER"<<endl;
+    cout<<"\t\t\t\"s\" for SCISSOR"<<endl;
+    cout<<"Press your choice then hit ENTER!"<<endl<<endl;
+    cout<<"Let's GO!"<<endl;
 }
 
 void userinputs(){
-    
+
+}
+
+void output(){
+
+}
+
+int main(){
+
+    dashboard();
+    return 0;
 }
